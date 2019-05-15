8473a8ed0f230c11e12e73bdacd659ef4b995ec7
Stenio Wagner <stenio.wagner1@gmail.com>
2019-05-15 13:36:53 -0300

    feature: creating the feature that allows user to zoom images on place detail screen

--- src/components/common/place-detail/components/sections/images-list/ImagesList.js
+++ src/components/common/place-detail/components/sections/images-list/ImagesList.js
@@ -1,13 +1,15 @@
 // @flow
 
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { FlatList } from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
+
 import styled from 'styled-components';
 
+import ImagesListWithZoom from './ImagesListWithZoom';
 import PlaceImagesListItem from './ImagesListItem';
 
 const ImagesList = styled(FlatList)`
-  flex: 1;
   width: 100%;
   height: ${({ theme }) => theme.metrics.getWidthFromDP('50%')}px;
   margin-top: ${({ theme }) => theme.metrics.extraLargeSize}px;
@@ -15,24 +17,70 @@ const ImagesList = styled(FlatList)`
 `;
 
 const IMAGES = [
-  'https://s3-sa-east-1.amazonaws.com/bon-appetit-resources/restaurants/medium/coco-bambu-sul.jpeg',
-  'https://s3-sa-east-1.amazonaws.com/bon-appetit-resources/restaurants/medium/misaki.jpeg',
-  'https://s3-sa-east-1.amazonaws.com/bon-appetit-resources/restaurants/medium/cabana-riomar.jpeg',
+  {
+    url:
+      'https://s3-sa-east-1.amazonaws.com/bon-appetit-resources/restaurants/medium/coco-bambu-sul.jpeg',
+  },
+  {
+    url:
+      'https://s3-sa-east-1.amazonaws.com/bon-appetit-resources/restaurants/medium/misaki.jpeg',
+  },
+  {
+    url:
+      'https://s3-sa-east-1.amazonaws.com/bon-appetit-resources/restaurants/medium/cabana-riomar.jpeg',
+  },
 ];
 
-const PlaceImagesList = (): Object => (
-  <ImagesList
-    keyExtractor={imageURL => imageURL}
-    showsHorizontalScrollIndicator={false}
-    data={IMAGES}
-    horizontal
-    renderItem={({ item, index }) => (
-      <PlaceImagesListItem
-        isFirst={index === 0}
-        imageURL={item}
-      />
-    )}
-  />
-);
+class PlaceImagesList extends Component {
+  state = {
+    isModalImagesOpen: false,
+    indexImageSelected: 0,
+  };
+
+  onToggleModalImages = (): void => {
+    const { isModalImagesOpen } = this.state;
+
+    this.setState({
+      isModalImagesOpen: !isModalImagesOpen,
+    });
+  };
+
+  onPressImage = (indexImageSelected: number): void => {
+    this.setState({
+      isModalImagesOpen: true,
+      indexImageSelected,
+    });
+  };
+
+  render() {
+    const { indexImageSelected, isModalImagesOpen } = this.state;
+
+    return (
+      <Fragment>
+        <ImagesList
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={item => item.url}
+          data={IMAGES}
+          horizontal
+          renderItem={({ item, index }) => (
+            <PlaceImagesListItem
+              onPressImage={() => this.onPressImage(index)}
+              isFirst={index === 0}
+              imageURL={item.url}
+            />
+          )}
+        />
+        {isModalImagesOpen && (
+          <ImagesListWithZoom
+            onToggleModalImages={this.onToggleModalImages}
+            indexImageSelected={indexImageSelected}
+            isModalImagesOpen={isModalImagesOpen}
+            images={IMAGES}
+          />
+        )}
+      </Fragment>
+    );
+  }
+}
 
 export default PlaceImagesList;
--- src/components/common/place-detail/components/sections/images-list/ImagesListItem.js
+++ src/components/common/place-detail/components/sections/images-list/ImagesListItem.js
@@ -1,29 +1,44 @@
 // @flow
 
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import styled from 'styled-components';
 
-const PlaceImage = styled(FastImage).attrs(({ uri }) => ({
-  source: { priority: FastImage.priority.low, uri },
-}))`
+const Wrapper = styled(TouchableOpacity)`
   width: ${({ theme }) => theme.metrics.getWidthFromDP('90%')}px;
   height: 100%;
   margin-right: ${({ theme }) => theme.metrics.mediumSize}px;
   margin-left: ${({ isFirst, theme }) => (isFirst ? theme.metrics.mediumSize : 0)}px;
+`;
+
+const PlaceImage = styled(FastImage).attrs(({ uri }) => ({
+  source: { priority: FastImage.priority.low, uri },
+}))`
+  width: 100%;
+  height: 100%;
   border-radius: 6px;
 `;
 
 type Props = {
+  onPressImage: Function,
   isFirst: boolean,
   imageURL: string,
 };
 
-const PlaceImageListItem = ({ isFirst, imageURL }: Props): Object => (
-  <PlaceImage
+const PlaceImageListItem = ({
+  onPressImage,
+  imageURL,
+  isFirst,
+}: Props): Object => (
+  <Wrapper
+    onPress={onPressImage}
     isFirst={isFirst}
-    uri={imageURL}
-  />
+  >
+    <PlaceImage
+      uri={imageURL}
+    />
+  </Wrapper>
 );
 
 export default PlaceImageListItem;
