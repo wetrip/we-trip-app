3a915fc606a8497f26f43367b62630f2e07d565a
Stenio Wagner <stenio.wagner1@gmail.com>
2019-05-16 15:04:11 -0300

    feature: creating the list of tours at tours screen

---
+++ src/components/screens/tours/components/tour-detail/ToursList.js
@@ -0,0 +1,89 @@
+// @flow
+
+import React, { Fragment } from 'react';
+import { TouchableOpacity, FlatList, View } from 'react-native';
+import styled from 'styled-components';
+
+import DefaultPlaceListItem from '../../../../common/DefaultPlaceListItem';
+
+const Wrapper = styled(View)`
+  width: ${({ theme }) => theme.metrics.width}px;
+  background-color: ${({ theme }) => theme.colors.secondaryColor};
+`;
+
+const TimeLine = styled(View)`
+  width: ${({ theme }) => theme.metrics.extraSmallSize}px;
+  height: 100%;
+  margin-left: ${({ theme }) => theme.metrics.mediumSize}px;
+  background-color: ${({ theme }) => theme.colors.inactiveTabIcon};
+  position: absolute;
+`;
+
+const CircularMarker = styled(View)`
+  width: ${({ theme }) => theme.metrics.extraLargeSize}px;
+  height: ${({ theme }) => theme.metrics.extraLargeSize}px;
+  border-radius: ${({ theme }) => theme.metrics.extraLargeSize / 2}px;
+  background-color: ${({ theme }) => theme.colors.contrastColor};
+`;
+
+const ListItemWrapper = styled(View)`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  margin-vertical: ${({ theme }) => theme.metrics.mediumSize}px;
+`;
+
+const List = styled(FlatList)`
+  margin-left: ${({ theme }) => theme.metrics.extraSmallSize}px;
+`;
+
+const PlaceItemButton = styled(TouchableOpacity)`
+  width: ${({ theme }) => theme.metrics.width - theme.metrics.getWidthFromDP('10%')}px;
+`;
+
+type LatLng = {
+  longitude: number,
+  latitude: number,
+};
+
+type Place = {
+  distanceToUser: number,
+  location: LatLng,
+  imageURL: string,
+  isOpen: boolean,
+  name: string,
+  id: number,
+};
+
+type Props = {
+  onSelectPlace: Function,
+  places: Array<Place>,
+};
+
+const ToursList = ({ onSelectPlace, places }: Props): Object => (
+  <Wrapper>
+    <TimeLine />
+    <List
+      keyExtractor={item => `${item.id}`}
+      showsVerticalScrollIndicator={false}
+      data={places}
+      renderItem={({ item, index }) => (
+        <ListItemWrapper>
+          <CircularMarker />
+          <PlaceItemButton
+            onPress={() => onSelectPlace(item.id)}
+          >
+            <DefaultPlaceListItem
+              distanceToUser={item.distanceToUser}
+              imageURL={item.imageURL}
+              isOpen={item.isOpen}
+              name={item.name}
+            />
+          </PlaceItemButton>
+        </ListItemWrapper>
+      )}
+    />
+  </Wrapper>
+);
+
+export default ToursList;
--- src/components/screens/tours/routes.js
+++ src/components/screens/tours/routes.js
@@ -1,23 +1,58 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import { Platform } from 'react-native';
 
+import PlaceDetail from '../../common/place-detail/PlaceDetailContainer';
+import TourDetail from './components/tour-detail/TourDetail';
+import CONSTANTS from '../../../utils/CONSTANTS';
+import appStyles from '../../../styles';
 import Tours from './ToursContainer';
 
-const ROUTE_NAMES = {
+const LOCAL_ROUTES = {
+  TOUR_DETAIL: 'TOUR_DETAIL',
   TOURS: 'TOURS',
 };
 
 const RootStack = createStackNavigator(
   {
-    [ROUTE_NAMES.TOURS]: {
-      screen: Tours,
+    [LOCAL_ROUTES.TOURS]: {
+      screen: props => <Tours
+        {...props}
+        localRoutes={LOCAL_ROUTES}
+      />,
       navigationOptions: ({ navigation }) => ({
         title: 'Tours',
+        headerBackTitle: null,
+      }),
+    },
+
+    [LOCAL_ROUTES.TOUR_DETAIL]: {
+      screen: TourDetail,
+      navigationOptions: ({ navigation }) => ({
+        headerTintColor: appStyles.colors.textColor,
+        headerBackTitle: null,
+        headerStyle: {
+          backgroundColor: appStyles.colors.white,
+          borderBottomWidth: 0,
+          elevation: 0,
+        },
+      }),
+    },
+
+    [CONSTANTS.ROUTES.PLACE_DETAIL]: {
+      screen: PlaceDetail,
+      navigationOptions: () => ({
+        headerTintColor: appStyles.colors.textColor,
+        headerTransparent: true,
+        headerStyle: {
+          backgroundColor: 'transparent',
+          borderBottomWidth: 0,
+        },
       }),
     },
   },
   {
-    initialRouteName: ROUTE_NAMES.TOURS,
+    initialRouteName: LOCAL_ROUTES.TOURS,
     mode: Platform.OS === 'ios' ? 'card' : 'modal',
     headerMode: 'screen',
   },
