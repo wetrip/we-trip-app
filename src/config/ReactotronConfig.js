0c9b350383d0975208bc47cf44a9f345873d2b97
Stenio Wagner <stenio.wagner1@gmail.com>
2019-05-18 13:33:24 -0300

    feat: Setting up Redux and Redux-Saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,12 @@
-/* global __DEV__ */
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga';
 
 if (__DEV__) {
   const tron = Reactotron.configure()
     .useReactNative()
+    .use(reactotronRedux())
+    .use(sagaPlugin())
     .connect();
 
   tron.clear();
